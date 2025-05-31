5f19494f888c471610cf69346425548ac56955e7
SayLucifer <95260082+SayLucifer@users.noreply.github.com>
2025-05-31 15:36:53 +0100

    Add .next/static/chunks/src_app_surah_[surahId]_page_tsx_a993527f._.js 1748702213026

---
+++ .next/static/chunks/src_app_surah_[surahId]_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d183aaae._.js",
+  "static/chunks/src_3e9c4b16._.js"
+],
+    source: "dynamic"
+});
